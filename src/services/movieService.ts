e7193d08c2283c33d3aa5642b5667cf4dbd912a7
Vadym <vadim.drinevskiy@gmail.com>
2025-05-24 21:22:04 +0300

    modified movieService.ts

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { type Movie } from "../types/movie";
 
+interface MovieResponce {
+    results: Movie[];
+}
+
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
     console.log(import.meta.env.VITE_VITE_TMDB_TOKEN);
     
-    const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+    const response = await axios.get<MovieResponce>('https://api.themoviedb.org/3/search/movie', {
         params: {
             query,
             include_adult: false,
